ee773d8046f60c8e008add98378842e2286bbd8c
heraldblrt <herald.balerite.work@gmail.com>
2024-05-18 01:13:54 -0400

    Updated the Main Page and Added Projects todo

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Herald Cloud Resume v1.0.0 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2024 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/herald-resume/blob/master/LICENSE)
 */
 //
 // Scripts
